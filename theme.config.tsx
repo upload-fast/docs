52e1089e47dfaeead564df5098be77ab8f588de7
iyifr <adekileiyimide@gmail.com>
2025-01-22 17:08:51 +0100

    feat(content): completed intro page

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>UploadFast</span>,
+  logo: (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <img
+        style={{ width: "40px" }}
+        src="/uploadfast.webp"
+        alt="Uploadfast logo"
+      />
+      <span>Upload-Fast <small>docs</small></span>
+    </div>
+  ),
   project: {
     link: 'https://github.com/upload-fast',
   },
